256e2e3b0767986a69b130ec73d82ca1e60b98b9
Peter Craftsmn <peter.singh@mail.utoronto.ca>
2022-02-25 14:00:12 -0500

    admin role added

--- test/jwtRoles.test.js
+++ test/jwtRoles.test.js
@@ -4,6 +4,27 @@ const jwtRoles = require( '../lib/jwtRoles' );
 
 describe( "Lib jwt Roles", function () {
 
+    it( "getJwtAdminRoleCode", function () {
+
+        const roleItemEditorCode = "ad";
+
+        // Act
+        const receivedCode = jwtRoles.getJwtAdminRoleCode();
+
+        // Assert
+        assert.deepStrictEqual( receivedCode, roleItemEditorCode );
+    } );
+
+    it( "getNameOfAdminRoleFromCode", function () {
+
+        const roleItemEditorCode = "ad";
+
+        // Act
+        const receivedCode = jwtRoles.getNameOfRoleFromCode( roleItemEditorCode );
+
+        // Assert
+        assert.deepStrictEqual( receivedCode, "admin" );
+    } );
 
     it( "getJwtEditorRoleCode", function () {
 
